293db1af43136ad5297d9cc8291a605fdd2d148f
Andrii Dovhan <dovhan.andriy@gmail.com>
2019-03-05 12:37:23 +0200

    rename method in tests setUpOnce -> setupOnce

--- services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/grpc/LogSpec.groovy
+++ services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/grpc/LogSpec.groovy
@@ -34,7 +34,7 @@ class LogSpec extends BaseSpecification {
     @Shared
     String switchIp
 
-    def setUpOnce() {
+    def setupOnce() {
         requireProfiles("hardware")
         def nFlowSwitch = northbound.activeSwitches.find { it.description =~ /NW[0-9]+.[0-9].[0-9]/ }
         def pattern = /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\-){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/
--- services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/grpc/LogicalPortSpec.groovy
+++ services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/grpc/LogicalPortSpec.groovy
@@ -28,7 +28,7 @@ class LogicalPortSpec extends BaseSpecification {
     @Shared
     Integer switchLogicalPort
 
-    def setUpOnce() {
+    def setupOnce() {
         requireProfiles("hardware")
         def nFlowSwitch = northbound.activeSwitches.find { it.description =~ /NW[0-9]+.[0-9].[0-9]/ }
         def pattern = /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\-){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/
